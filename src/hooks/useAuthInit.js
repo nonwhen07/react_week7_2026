9971aa54538b10b9d75e597acdc6ebe0c242351f
nonwhen07 <30990876+nonwhen07@users.noreply.github.com>
2026-03-12 12:50:27 +0800

    feat: 抽出登入邏輯並建立Custom Hook

--- src/hooks/useAuthInit.js
+++ src/hooks/useAuthInit.js
@@ -1,5 +1,5 @@
-// import BackHeader from '@/components/admin/BackHeader';
-// import PageLoader from '@/components/PageLoader';
+import { Outlet } from 'react-router-dom';
+
 import AdminSidebar from '@/components/admin/AdminSidebar';
 import AdminNavbar from '@/components/admin/AdminNavbar';
 import GoTop from '@/components/GoTop';
@@ -7,26 +7,23 @@ import GoTop from '@/components/GoTop';
 import useAuthInit from '@/hooks/useAuthInit';
 
 export default function AdminLayout() {
-  // Custom Hook = 可重用的「邏輯」，而不是 UI。
-  // 判斷邏輯：① 有 useState / useEffect / useMemo / useCallback
-  // ② 跟 UI 無關、③ 可能在別的頁面也會用、④ 讓 Component 太長，所以這段可以抽出去當 Custom Hook
   // 初始化登入狀態
   useAuthInit();
 
   return (
     <>
       <div className="d-flex">
-        {/* Sidebar */}
         <AdminSidebar />
+
         <div className="flex-grow-1">
-          {/* Top Navbar */}
           <AdminNavbar />
-          {/* Page Content */}
+
           <main className="ui-layout container p-4 bg-light min-vh-100">
             <Outlet />
           </main>
         </div>
       </div>
+
       <GoTop />
     </>
   );
